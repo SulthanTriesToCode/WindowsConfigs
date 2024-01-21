7c92f1a2394baee31976db679ad0dfc8424036d8
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-21 17:06:08 +0700

    komorebic stuff

---
+++ MoveConfig.ps1
@@ -0,0 +1,3 @@
+mkdir "$Env:USERPROFILE\.config" -ea 0
+move "whkdrc" "$Env:USERPROFILE\.config"
+move "komorebi.json" "$Env:USERPROFILE\"
---
+++ ScoopNKomorebicSetup.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+scoop install git
+scoop bucket add extras
+scoop install whkd
+scoop install komorebi
+
+komorebic fetch-app-specific-configuration
